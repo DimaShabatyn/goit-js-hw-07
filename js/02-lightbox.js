1d06c02550735715707fb2167cf33a6346a04b07
DimaShabatyn <dmitriyshabatyn@gmail.com>
2023-02-16 15:07:20 +0200

    Task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//отримати доступ до ul
+const gallery = document.querySelector('.gallery');
+//перебираємо масив для отримання одного зображення
+// та інші по черзі, а також створюємо Li
+// galleryItems.map(({preview, original, description}) => {
+// `<li>
+// <a class="gallery__item" href="${original}">
+// <img class="gallery__image" src="${preview}" alt="${description}" />
+// </a>
+// </li>`
+// }).join('')
+// console.log(galleryItems);
+
+//присвоюємо в змінну результат створеного масиву - довгий рядок з li
+// const markup = galleryItems.map(({preview, original, description}) => {
+//     return `<li>
+//     <a class="gallery__item" href="${original}">
+//     <img class="gallery__image" src="${preview}" alt="${description}" />
+//     </a>
+//     </li>`
+//     }).join('');
+
+// console.log(galleryItems);
+// console.log(markup);
+
+//неявне повернення
+    const markup = galleryItems.map(({preview, original, description}) => `<li>
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li>`).join('');
+        console.log(markup);
+
+//вставимо розмітку-рядок в ul
+// gallery.insertAdjacentHTML('beforeend', markup);
+gallery.innerHTML = markup;
+
+//додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу 
+//і з'являється через 250 мілісекунд після відкриття зображення.
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt', 
+    captionPosition: 'bottom', 
+    captionDelay: 250, });
